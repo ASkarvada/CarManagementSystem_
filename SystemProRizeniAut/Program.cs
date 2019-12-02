c7860c5cba231537f8fc1fac4a4bea7c44066bd7
Škarvada Adam <skarvada.adam.2017@skola.ssps.cz>
2019-12-02 16:16:28 +0100

    Přidejte soubory projektu.

---
+++ SystemProRizeniAut/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemProRizeniAut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+
+
+    class Trasa
+    {
+        
+        
+
+    }
+
+    public enum Teren { most, tunel, silnice }
+
+    public List<Trasa> Tr = new List<Trasa>();
+
+    class SMD
+    {
+        public enum Pocasi { dest, slunecno, kroupy, zatazeno, bourka }
+    }
+
+    class RC
+    {
+
+    }
+
+    class AV
+    {
+        public delegate void Vozidlo(double rychlost, double delka_trasy, Teren t, bool svetla, List<Trasa> Tr);
+    }
+
+
+}
